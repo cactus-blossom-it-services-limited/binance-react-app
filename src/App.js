454b2e647665866e54a4e97ae1abd92924298d90
andrew farquharson <farquharson.a@gmail.com>
2021-05-22 19:18:57 +0100

    typo

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import Coin from './Coin';
 
 function App() {
   const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => {
       setCoins(res.data);
-    }).catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
   }, []);
 
   const handleChange = e => {
